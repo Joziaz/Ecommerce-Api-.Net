33d750fef4cee8d39fbe1574aeb84c76a984f6b9
Jozi <josiasduran51@gmail.com>
2021-03-30 21:17:31 -0400

    add base controller

--- Shared/Infrastructure/BaseController.cs
+++ Shared/Infrastructure/BaseController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using api.Shared.Domain;
+using api.Shared.Domain.Exeptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Shared.Infrastructure
@@ -19,6 +20,48 @@ namespace api.Shared.Infrastructure
             var all = _repository.GetAll();
 
             return Ok(await all);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _repository.Get(id);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] T instance)
+        {
+            await _repository.Create(instance);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] T instance)
+        {   
+            instance.Id = id;
+            try
+            {
+                await _repository.Update(instance);
+            }
+            catch (InvalidRegistryExecption)
+            {
+                return NotFound("Invalid Id");
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {   
+            await _repository.Delete(id);
+            return Ok();
         }
     }
 }
