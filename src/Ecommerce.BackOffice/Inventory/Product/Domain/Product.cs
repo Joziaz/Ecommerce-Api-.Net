5700dbd3e9e0b7049d6e641422d804eb27a256f1
Jozi <josiasduran51@gmail.com>
2021-08-07 12:30:22 -0400

    change price fields
    change the price fields logic to get both of them separate

--- src/Ecommerce.BackOffice/Inventory/Product/Domain/Product.cs
+++ src/Ecommerce.BackOffice/Inventory/Product/Domain/Product.cs
@@ -8,26 +8,18 @@ namespace Ecommerce.BackOffice.Inventory.Domain
         public string Name { get; private set; }
         public string Description { get; set; }
         public Category Category { get; set; }
-        private decimal _price { get; set; }
-        public decimal Price { get => GetPrice(); }
+        public decimal Price { get; private set;}
         public decimal DiscountPrice { get; private set; }
 
         public Product(string name, decimal price)
         {
             Name = name;
-            _price = price;
+            Price = price;
         }
 
-        private decimal GetPrice()
-        {
-            if (DiscountPrice != 0)
-                return DiscountPrice;
-
-            return _price;
-        }
         public void SetDiscountPrice(decimal discount)
         {
-            if (discount >= _price)
+            if (discount >= Price)
                 throw new DiscountPriceInvalid("the discount price can't be gather or equal to the normal price");
 
             DiscountPrice = discount;
